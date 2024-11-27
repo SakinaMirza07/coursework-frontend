d48775f17c789877db480cd8ec6b6aab14a05df6
Sakina Mirza <sakina34540@gmail.com>
2024-11-27 13:32:42 +0400

    This is my frontend

---
+++ products.js
@@ -0,0 +1,102 @@
+let products = [
+    {
+        id: 2001,
+        title: "Swimming",
+        description: "Learn the fundamentals of <em>freestyle</em>, <em>butterfly</em>, and <em>backstroke</em> swimming techniques.",
+        availability:5,
+        price: 100,
+        image: "images/swimming.avif",
+        availableInventory: 10,
+        rating: 5
+    },
+    {
+        id: 2002,
+        title: "Karate",
+        description: "Master the art of <em>karate</em> through discipline, self-defense techniques, and physical conditioning.",
+        availability:5,
+        price: 100,
+        image: "images/karate.avif",
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id: 2003,
+        title: "Basketball",
+        description: "Improve your <em>dribbling</em>, <em>shooting</em>, and <em>teamwork</em> skills in basketball.",
+        availability:5,
+        price: 100,
+        image: "images/basketball.avif",
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id: 2004,
+        title: "Dance",
+        description: "Explore various <em>dance styles</em>, including hip-hop, contemporary, and ballroom.",
+        availability:5,
+        price: 100,
+        image: "images/dance.avif",
+        availableInventory: 10,
+        rating: 5
+    },
+    {
+        id: 2005,
+        title: "Football",
+        description: "Learn the rules, <em>skills</em>, and <em>strategies</em> of football, from passing to team play.",
+        availability:5,
+        price: 90,
+        image: "images/football.avif",
+        availableInventory: 10,
+        rating: 5
+    },
+    {
+        id: 2006,
+        title: "Music",
+        description: "Develop skills in playing <em>instruments</em>, reading <em>music sheets</em>, and performing.",
+        availability:5,
+        price: 80,
+        image: "images/music.avif",
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id: 2007,
+        title: "Painting",
+        description: "Explore <em>painting</em>, <em>sculpture</em>, and <em>crafting</em> to express creativity.",
+        availability:5,
+        price: 80,
+        image: "images/painting.avif",
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        id: 2008,
+        title: "Yoga",
+        description: "Focus on <em>mind-body connection</em>, flexibility, and breathing techniques in yoga.",
+        availability:5,
+        price: 60,
+        image: "images/yoga.avif",
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id: 2009,
+        title: "Tennis",
+        description: "Refine your skills in <em>serve</em>, <em>volley</em>, and <em>court positioning</em> in tennis.",
+        availability:5,
+        price: 70,
+        image: "images/tennis.avif",
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id: 2010,
+        title: "Photography",
+        description: "Learn the basics of <em>camera techniques</em>, composition, and editing in photography.",
+        availability:5,
+        price: 50,
+        image: "images/photography.avif",
+        availableInventory: 10,
+        rating: 5
+    }
+];
